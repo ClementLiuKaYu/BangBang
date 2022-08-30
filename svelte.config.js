586369db38dce3072b593f5a9f3e573ee6eb4647
Clement <clement.liukayu@gmail.com>
2022-08-31 01:22:14 +0800

    changed package.json

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,9 @@ import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {}
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
